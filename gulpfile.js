2315e95d5fb21654e9e3bd3027e0f3050d42cf99
Locke <moyerock@gmail.com>
2016-03-19 15:58:57 +0800

    introduce babel with the webpack & gulp group

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,15 @@ gulp.task('build', function () {
             module: {
                 loaders: [
                     {test: /\.css$/, loader: 'style!css'},
-                    {test: /\.js|jsx$/, loader: 'jsx?harmony'}
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader',
+                        exclude: /node_modules/,
+                        query: {
+                            cacheDirectory: true,
+                            presets: ['es2015', 'react']
+                        }
+                    }
                 ]
             }
         }))
