c3b20494c8f4e19fd24b18d71efa0b6a00e4c128
Locke <moyerock@gmail.com>
2016-04-18 16:56:50 +0800

    simple reirection.

--- Client/Components/default.js
+++ Client/Components/default.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { browserHistory } from 'react-router'
 
 const Default = React.createClass({
+    handleRedirect: function () {
+        browserHistory.push('/form')
+    },
     render: function () {
         return (
-            <quote>路由演示</quote>
+            <div>
+                <quote>路由演示</quote>
+                <br/>
+                <input
+                    type="button"
+                    value="Redirect"
+                    onClick={this.handleRedirect}
+                />
+            </div>
         );
     }
 });
